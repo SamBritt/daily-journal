1796ef324d8323654489204e229c6021c6517a0b
SamBritt <sam.britt2@yahoo.com>
2019-03-01 14:27:36 -0600

    added js file for querySelector

---
+++ src/scripts/journal.js
@@ -0,0 +1,18 @@
+
+const entries = [];
+
+const entryDate = document.querySelector('#journalDate');
+const entryTitle = document.querySelector('#entryTitle');
+const mainEntry = document.querySelector('#mainEntry');
+const entryMood = document.querySelector('#entryMood');
+
+const journalEntry = {
+    date: this.entryDate,
+    concepts: this.entryTitle,
+    entry: this.mainEntry,
+    mood: this.entryMood
+}
+
+
+
+
